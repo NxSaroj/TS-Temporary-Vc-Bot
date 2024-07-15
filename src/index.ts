901be0a6542618551cee75e11f65178d29191c30
Syst69 <148795898+Syst69@users.noreply.github.com>
2024-07-15 19:18:08 +0530

    80% completed

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { CommandKit } from 'commandkit'
 import  { loadEvents } from './utils/handlers/loadEvents'
 import { connect } from 'mongoose'
 import path from 'node:path'
+import colors from 'colors'
+import 'dotenv/config'
 
 
 const client = new Client({
@@ -22,7 +24,7 @@ new CommandKit({
     commandsPath: path.join(__dirname, 'commands')
 })
 
-connect("mongodb+srv://Syst:xoderz@xoderz.cz4cjej.mongodb.net/?retryWrites=true&w=majority").then((c) => console.log("MongoDB Is Connected")).catch((e) => console.dir(e))
+connect(process.env.DB_URI).then((c) => console.log(colors.green("MongoDB Connected"))).catch((e) => console.error(e))
 
-client.login("MTE4NTI2NDk2OTI1MjYxNDIwNA.GFeJNp._bcrNz6cXB5PeBZBAMU66K4Zi01ai04pqiovP0")
+client.login(process.env.TOKEN)
 
